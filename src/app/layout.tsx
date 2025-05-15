b189f623f1313d607fbc9b616bc93fa1420c5f15
Sahil Shangloo <sahilshangloo35@gmail.com>
2025-05-15 08:42:15 +0530

    Added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
         >
             <html lang="en">
                 <head>
-                    <link rel="icon" href="/CloakMe_ico.ico" sizes="any" />
+                    <link rel="icon" href="/favicon.ico" sizes="any" />
                 </head>
                 <body
                     className={`${geistSans.variable} ${geistMono.variable} antialiased`}
